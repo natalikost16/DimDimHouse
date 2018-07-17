bee80212073746ca619066c38d4065d5d6bb9096
Natali_sofa <natali_sofa@ukr.net>
2018-07-17 21:05:39 +0000

    css html

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.smooth').on('click', function(event) {
+        let target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
+
